9a1ec7c0d04770f7653b63a56fcadc42c2260c4c
triley22 <triley09@gmail.com>
2017-02-24 14:54:03 -0800

    review scaffold

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -22,7 +22,7 @@ class MoviesController < ApplicationController
     respond_to do |format|
       if @movie.save
         format.html { redirect_to @movie, notice: 'Movie was successfully created.' }
-        format.json { render :show, status: :created, location: @movie }
+        format.json { render :show, status: :created, location: @movie } 
       else
         format.html { render :new }
         format.json { render json: @movie.errors, status: :unprocessable_entity }
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :movies
+  has_many :reviews, dependent: :destroy
 end
